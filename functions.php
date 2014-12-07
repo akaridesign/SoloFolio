e093951a004973bfeb375467369ea5d2826c7bf1
Joel Hawksley <joel@mojotech.com>
2014-12-06 20:38:28 -0500

    [FIX] Disable vertical centering on Horizon.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define("SOLOFOLIO_VERSION",     "7.0.57");
+define("SOLOFOLIO_VERSION",     "7.0.58");
 
 include_once("includes/helpers.php");             // Helper functions
 include_once("includes/gallery.php");             // Gallery shortcode replacement
