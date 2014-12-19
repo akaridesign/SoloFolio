85b7585aae354de6c39b616439d95d302445fb97
Joel Hawksley <joel@mojotech.com>
2014-12-19 14:21:53 -0500

    [REVIEW CLEANUP] Translate footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 </div>
 
 <div class="footer">
-  <span class="copyright-label">copyright</span>
+  <span class="copyright-label"><?php _e( 'copyright', 'solofolio' ); ?></span>
   <div class="copyright-hidden">
     <?php $text = get_theme_mod( 'solofolio_copyright_text' ); if (!empty($text)) { ?>
       &copy; <?php echo date("Y"); ?> <?php echo get_theme_mod( 'solofolio_copyright_text' ); ?>
     <?php } if (get_theme_mod('solofolio_show_attribution', true)) { ?>
-      / Powered by <a title="Powered by SoloFolio, a WordPress Photography Portfolio Theme" href="http://solofol.io" target="_blank">SoloFolio</a>
+      / <?php _e( 'Powered by', 'solofolio' ); ?> <a title="<?php _e( 'Powered by SoloFolio, a WordPress Photography Portfolio Theme', 'solofolio' ); ?>" href="http://solofol.io" target="_blank"> <?php _e( 'SoloFolio', 'solofolio' ); ?></a>
     <?php } ?>
   </div>
 </div>
