a86295a9ad406961ec88d4dcaa7b8283232d5d31
Joel Hawksley <joel@mojotech.com>
2015-03-28 11:03:00 -0400

    7.0.6 version bump.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define("SOLOFOLIO_VERSION",     "7.0.105");
+define("SOLOFOLIO_VERSION",     "7.0.106");
 
 include_once("includes/helpers.php");             // Helper functions
 include_once("includes/gallery.php");             // Gallery shortcode replacement
